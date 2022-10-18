de5a31653b8fb2de2420941ef63ebba9e2e6ae35
Br Arth <107882082+BrunoArth@users.noreply.github.com>
2022-10-18 14:08:46 -0300

    vetores 2.0

--- Exercicio34/ex34.c
+++ Exercicio34/ex34.c
@@ -1,1 +1,27 @@
-﻿
+﻿#include <stdio.h>
+
+//Contar vogais
+
+int main(void){
+	char vogais[] = "aeiouAEIOU";
+	char palavra[10];
+	int contVogais = 0;
+	
+	printf("Digite a palavra\n");
+	scanf("%s", &palavra);
+
+	for(int i = 0; palavra[i]!='\0'; i++){
+		for(int j = 0; vogais[j]!='\0'; j++){
+			if(palavra[i]==vogais[j]){
+				contVogais++;
+				break;
+			}
+		}
+	}
+	printf("A quantidade de vogais na palavra foi de, %d\n", contVogais);
+}
+		
+
+
+
+
