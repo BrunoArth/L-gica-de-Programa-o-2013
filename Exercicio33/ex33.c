e5f8f538ae1d11887b051808d842b2ade949a4c9
Br Arth <107882082+BrunoArth@users.noreply.github.com>
2022-10-18 09:55:10 -0300

    Vetores

---
+++ Exercicio33/ex33.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+//Inverter Palavras
+
+int main(void){
+	char palavra[20];
+	char palavraInvertida[20];
+	int cont = 0;
+	int qntdLetras = 0;
+
+	printf("Digite a palavra\n");
+	scanf("%s", &palavra);
+
+	for(int i = 0; palavra[i]!='\0'; i++){
+		qntdLetras++;	
+		}
+	qntdLetras--;
+
+	for(qntdLetras; qntdLetras>=0; qntdLetras--){
+		palavraInvertida[cont]=palavra[qntdLetras];
+		cont++;
+		}
+	printf("Palavra invertida, %s\n", &palavraInvertida);
+}
+		
+
+
+
+
